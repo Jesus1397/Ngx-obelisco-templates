1bf8bce57729f170f76b8893bc17b66d49311306
Jesus <jesus.fy.1997@gmail.com>
2024-07-04 12:38:37 -0300

    fix: goverment areas

--- src/app/modules/templates/goverment-areas-option-one/goverment-areas-option-one.component.ts
+++ src/app/modules/templates/goverment-areas-option-one/goverment-areas-option-one.component.ts
@@ -1,33 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OFooterComponent } from '@gcba/ngx-obelisco/footer';
+import { OFooterComponent, OFooterModule } from 'ngx-obelisco-example/footer';
 
-import { OCardModule } from '@gcba/ngx-obelisco/card';
-import { OButtonComponent } from '@gcba/ngx-obelisco/button';
-import { OAccessModule } from '@gcba/ngx-obelisco/access';
-import { PageHeaderComponent } from 'src/app/components/page-header/page-header.component' ;
-import { ODropdownModule } from '@gcba/ngx-obelisco/dropdown';
-import { DropdownNavigationItem, Media } from '@gcba/ngx-obelisco/core/models';
-import { BreadcrumbService } from '@gcba/ngx-obelisco/core/services';
-import { InstitutionalBlockComponent } from 'src/app/components/institutional-block/institutional-block.component' ;
-import { OHighlightedModule } from '@gcba/ngx-obelisco/highlighted';
+import { OCardModule } from 'ngx-obelisco-example/card';
+import { OButtonComponent, OButtonModule } from 'ngx-obelisco-example/button';
+import { OAccessModule } from 'ngx-obelisco-example/access';
+import { PageHeaderComponent } from 'src/app/components/page-header/page-header.component';
+import { ODropdownModule } from 'ngx-obelisco-example/dropdown';
+import {
+  DropdownNavigationItem,
+  Media,
+} from 'ngx-obelisco-example/core/models';
+import { BreadcrumbService } from 'ngx-obelisco-example/core/services';
+import { InstitutionalBlockComponent } from 'src/app/components/institutional-block/institutional-block.component';
+import { OHighlightedModule } from 'ngx-obelisco-example/highlighted';
 
 @Component({
   selector: 'app-goverment-areas',
   standalone: true,
   imports: [
     CommonModule,
-    OFooterComponent,
+    OFooterModule,
     OCardModule,
-    OButtonComponent,
+    OButtonModule,
     OAccessModule,
     PageHeaderComponent,
     ODropdownModule,
     InstitutionalBlockComponent,
-    OHighlightedModule
+    OHighlightedModule,
   ],
   templateUrl: './goverment-areas-option-one.component.html',
-  styleUrls: ['./goverment-areas-option-one.component.scss']
+  styleUrls: ['./goverment-areas-option-one.component.scss'],
 })
 export class GovermentAreasOptionOneComponent {
   public routes = [
@@ -54,68 +57,68 @@ export class GovermentAreasOptionOneComponent {
   //Inicio de Array de dropdown en el Hero de la pagina
   public dropdownItemsOne: DropdownNavigationItem[] = [
     {
-      text: "Ministerio de Cultura",
-      link: "/"
+      text: 'Ministerio de Cultura',
+      link: '/',
     },
     {
-      text: "Subsecretaría Gestión Cultural",
-      link: "/",
+      text: 'Subsecretaría Gestión Cultural',
+      link: '/',
     },
     {
-      text: "Subsecretaría de Políticas Culturales y Nuevas Audiencias",
-      link: "/",
+      text: 'Subsecretaría de Políticas Culturales y Nuevas Audiencias',
+      link: '/',
     },
     {
-      text: "Ente Autárquico Teatro Colón",
-      link: "/"
+      text: 'Ente Autárquico Teatro Colón',
+      link: '/',
     },
     {
-      text: "Organigrama",
-      link: "/",
+      text: 'Organigrama',
+      link: '/',
     },
     {
-      text: "Guia de Funcionarios",
-      link: "/",
+      text: 'Guia de Funcionarios',
+      link: '/',
     },
     {
-      text: "Contacto",
-      link: "/",
-    }
+      text: 'Contacto',
+      link: '/',
+    },
   ];
 
   public dropdownItemsTwo: DropdownNavigationItem[] = [
     {
-      text: "Escalas salariales",
-      link: "/"
+      text: 'Escalas salariales',
+      link: '/',
     },
     {
-      text: "Sueldos de funcionarios",
-      link: "/",
+      text: 'Sueldos de funcionarios',
+      link: '/',
     },
     {
-      text: "Concurso de personal",
-      link: "/",
+      text: 'Concurso de personal',
+      link: '/',
     },
     {
-      text: "Presupuesto abierto",
-      link: "/"
+      text: 'Presupuesto abierto',
+      link: '/',
     },
     {
-      text: "Compras y contrataciones de bienes y servicios",
-      link: "/",
+      text: 'Compras y contrataciones de bienes y servicios',
+      link: '/',
     },
     {
-      text: "Informes de auditoría",
-      link: "/",
+      text: 'Informes de auditoría',
+      link: '/',
     },
     {
-      text: "Trámites y servicios",
-      link: "/",
+      text: 'Trámites y servicios',
+      link: '/',
     },
     {
-      text: "Declaraciones juradas",
-      link: "/",
-    }
+      text: 'Declaraciones juradas',
+      link: '/',
+    },
   ];
   //Fin de Array de dropdown en el Hero de la pagina
 
@@ -123,12 +126,11 @@ export class GovermentAreasOptionOneComponent {
   public titleH: string = 'Destacado I';
   public titleHtwo: string = 'Destacado II';
 
-  public description: string = 'Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener negritas ni enlaces. Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener negritas ni enlaces';
+  public description: string =
+    'Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener negritas ni enlaces. Este es el cuerpo de un destacado. Debe ser breve y conciso, de pocas líneas. No puede contener negritas ni enlaces';
 
   public picture: Media = {
     src: 'https://gcba.github.io/obelisco-demo/assets/galeria/galeria-2.png',
-    alt: 'descripción de imagen'
   };
   //Fin de los datos de los componentes "Highlighted" en la pagina
 }
-
--- src/app/modules/templates/goverment-areas-option-two/goverment-areas-option-two.component.ts
+++ src/app/modules/templates/goverment-areas-option-two/goverment-areas-option-two.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OFooterComponent } from '@gcba/ngx-obelisco/footer';
+import { OFooterComponent, OFooterModule } from 'ngx-obelisco-example/footer';
 
-import { OCardModule } from '@gcba/ngx-obelisco/card';
-import { OButtonComponent } from '@gcba/ngx-obelisco/button';
-import { OAccessModule } from '@gcba/ngx-obelisco/access';
-import { PageHeaderComponent } from 'src/app/components/page-header/page-header.component' ;
-import { ODropdownModule } from '@gcba/ngx-obelisco/dropdown';
-import { DropdownNavigationItem } from '@gcba/ngx-obelisco/core/models';
-import { BreadcrumbService } from '@gcba/ngx-obelisco/core/services';
-import { InstitutionalBlockComponent } from 'src/app/components/institutional-block/institutional-block.component' ;
+import { OCardModule } from 'ngx-obelisco-example/card';
+import { OButtonComponent, OButtonModule } from 'ngx-obelisco-example/button';
+import { OAccessModule } from 'ngx-obelisco-example/access';
+import { PageHeaderComponent } from 'src/app/components/page-header/page-header.component';
+import { ODropdownModule } from 'ngx-obelisco-example/dropdown';
+import { DropdownNavigationItem } from 'ngx-obelisco-example/core/models';
+import { BreadcrumbService } from 'ngx-obelisco-example/core/services';
+import { InstitutionalBlockComponent } from 'src/app/components/institutional-block/institutional-block.component';
 
 @Component({
   selector: 'app-goverment-areas',
   standalone: true,
   imports: [
     CommonModule,
-    OFooterComponent,
+    OFooterModule,
     OCardModule,
-    OButtonComponent,
+    OButtonModule,
     OAccessModule,
     PageHeaderComponent,
     ODropdownModule,
-    InstitutionalBlockComponent
+    InstitutionalBlockComponent,
   ],
   templateUrl: './goverment-areas-option-two.component.html',
-  styleUrls: ['./goverment-areas-option-two.component.scss']
+  styleUrls: ['./goverment-areas-option-two.component.scss'],
 })
 export class GovermentAreasOptionTwoComponent {
   public routes = [
@@ -52,34 +52,33 @@ export class GovermentAreasOptionTwoComponent {
   //Inicio de Array de dropdown en el Hero de la pagina
   public dropdownItem: DropdownNavigationItem[] = [
     {
-      text: "Ministerio de Cultura",
-      link: "/"
+      text: 'Ministerio de Cultura',
+      link: '/',
     },
     {
-      text: "Subsecretaría Gestión Cultural",
-      link: "/",
+      text: 'Subsecretaría Gestión Cultural',
+      link: '/',
     },
     {
-      text: "Subsecretaría de Políticas Culturales y Nuevas Audiencias",
-      link: "/",
+      text: 'Subsecretaría de Políticas Culturales y Nuevas Audiencias',
+      link: '/',
     },
     {
-      text: "Ente Autárquico Teatro Colón",
-      link: "/"
+      text: 'Ente Autárquico Teatro Colón',
+      link: '/',
     },
     {
-      text: "Organigrama",
-      link: "/",
+      text: 'Organigrama',
+      link: '/',
     },
     {
-      text: "Guia de Funcionarios",
-      link: "/",
+      text: 'Guia de Funcionarios',
+      link: '/',
     },
     {
-      text: "Contacto",
-      link: "/",
-    }
+      text: 'Contacto',
+      link: '/',
+    },
   ];
   //Fin de Array de dropdown en el Hero de la pagina
 }
-
